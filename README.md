2d185fc5d065ed981dd5c25d0c379d1db5c50d13
karbowskam <66951357+karbowskam@users.noreply.github.com>
2021-01-27 13:46:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Installation
+
+Simple toolkit for my static websites development.
+
+## How to use it?
+
+Go to https://github.com/karbowskam/my-starter-kit (this repository) and click on `Use this template` button to generate a new repository based on this starter kit. 
+After that clone it to your computer, go to the project directory in console and type `npm install`.
+
+## Available scripts
+
+`npm run start` - runs development mode
+
+`npm run build` - runs build process for production
+
+`npm run publish` - runs build process and publish the page using `gh-pages` branch
---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+// Imports
+const gulp =          require('gulp');
+const eslint =        require('gulp-eslint');
+const cssValidator =  require('gulp-w3c-css');
+const htmlValidator = require('gulp-w3c-html-validator');
+const htmllint =      require('gulp-htmllint');
+const fancyLog =      require('fancy-log');
+const colors =        require('ansi-colors');
+var map =             require('map-stream');
+
+const srcPathCSS =    "src/scss/*.scss";
+const srcPathHTML =   "src/**/*.html";
+
+// Tasks
+const task = {
+   validateJSLint() {
+      return gulp.src(['src/js/*.js'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError()); 
+   },
+   validateCSS(){
+      return gulp.src(srcPathCSS)
+         .pipe(cssValidator())
+         .pipe(map(function(file, done) {
+            if (file.contents.length > 0) {
+               var issues = JSON.parse(file.contents.toString());
+               if(issues.errors.length > 0){
+                  issues.errors.forEach(function (issue) {
+                     fancy("gulp-css", issue.message, file.path, issue.line, "", issue.errorType);
+                  });
+                  process.exitCode = 1;
+               }
+               
+               issues.warnings.forEach(function (issue) {
+                  fancy("gulp-css", issue.message, file.path, issue.line, "", issue.errorType);
+                });
+            }
+            done(null, file);
+          }));
+   },
+   validateHtml() {
+      return gulp.src(srcPathHTML)
+         .pipe(htmlValidator())
+         .pipe(htmlValidator.reporter());
+   },
+   validateHtmlLint(){
+      return gulp.src(srcPathHTML)
+         .pipe(htmllint({}, htmllintReporter));
+   }
+};
+
+function fancy(module, msg, filepath, line, column, code){
+   fancyLog(colors.cyan('[' + module + '] ') + colors.white(filepath + ' [' + line + ',' + column + ']: ') + colors.red('(' + code + ') ' + msg));
+}
+
+function htmllintReporter(filepath, issues) {
+   if (issues.length > 0) {
+       issues.forEach(function (issue) {
+         fancy("gulp-htmllint", issue.msg, filepath, issue.line, issue.column, issue.code)
+       });
+
+       process.exitCode = 1;
+   }
+}
+
+// Gulp
+gulp.task('validate-js-lint', task.validateJSLint);
+gulp.task('validate-html', task.validateHtml);
+gulp.task('validate-html-lint', task.validateHtmlLint);
+gulp.task('validate-css', task.validateCSS);
